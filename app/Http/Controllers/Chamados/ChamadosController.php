cc1d6d23022a5e09e3604a97b33e021b123629a1
Victor Iki <victooriki@gmail.comm>
2024-01-10 01:09:39 -0300

    view e edit com put chamd

--- app/Http/Controllers/Chamados/ChamadosController.php
+++ app/Http/Controllers/Chamados/ChamadosController.php
@@ -8,25 +8,65 @@ use Illuminate\Http\Request;
 
 class ChamadosController extends Controller
 {
-    public function index(Chamados $chamados_model)
-    {
-        $chamados = $chamados_model->all();
+    protected $chamados;
 
-        return view('chamados/index', compact('chamados'));
+    public function __construct(Chamados $chamados)
+    {
+        $this->chamados = $chamados;
+    }
+
+    public function index()
+    {
+        $chamados = $this->chamados->all();
+
+        return view('chamados.index', compact('chamados'));
     }
 
     public function create()
     {
 
-        return view('chamados/create');
+        return view('chamados.create');
     }
 
-    public function insert_chamado(Request $request, Chamados $chamados_model)
+    public function insert_chamado(Request $request)
     {
         $data = $request->all();
         $data['stats'] = 1;
 
-        $chamados_model->create($data);
+        $this->chamados->create($data);
+
+        return redirect()->route('chamados.index');
+    }
+
+    public function show(string|int $id)
+    {
+        if(!$chamado = $this->chamados->find($id)) {
+            return back();
+        }
+
+        return view('chamados.show', compact('chamado'));
+    }
+
+    public function edit(string|int $id)
+    {
+        if(!$chamado = $this->chamados->find($id)) {
+            return back();
+        }
+
+        return view('chamados.edit', compact('chamado'));
+    }
+
+    public function update(Request $request, string|int $id)
+    {
+        if(!$chamado = $this->chamados->find($id)) {
+            return back();
+        }
+
+        $chamado->update($request->only([
+            'titulo',
+            'descricao',
+            'email'
+        ]));
 
         return redirect()->route('chamados.index');
     }
---
+++ resources/views/chamados/show.blade.php
@@ -0,0 +1,16 @@
+<h1>Detalhes do Chamado {{ $chamado->titulo }}</h1>
+
+<ul>
+    <li>{{ $chamado->titulo }}</li>
+    <li>{{ $chamado->descricao }}</li>
+    <li>{{ $chamado->email }}</li>
+    <li>{{ $chamado->stats }}</li>
+    @if($chamado->stats == 2)
+        <li>{{ $chamado->data_fechamento }}</li>
+    @endif
+    @if($chamado->stats == 3)
+        <li>{{ $chamado->data_conclusao}}</li>
+        <li>{{ $chamado->avaliacao}}</li>
+        <li>{{ $chamado->feedback_avaliacao}}</li>
+    @endif
+</ul>
