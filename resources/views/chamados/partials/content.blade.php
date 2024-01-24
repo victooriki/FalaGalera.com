971687c2fc966660ed9a1b99bae360baba7b2366
Victor Iki <victooriki@gmail.comm>
2024-01-24 01:19:28 -0300

    ajuste dck file e desc

--- resources/views/chamados/partials/content.blade.php
+++ resources/views/chamados/partials/content.blade.php
@@ -14,7 +14,7 @@
                             </th>
 
                             <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                Comentário
+                                Descrição
                             </th>
 
                             <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">Interações</th>
